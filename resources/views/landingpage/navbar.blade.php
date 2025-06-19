3baab117c69ccac90416c3e126befa3f882d865f
teguhafrianda <teguhafrianda8@gmail.com>
2025-06-19 17:38:18 +0700

    Devide module layouts landingpage

---
+++ resources/views/landingpage/navbar.blade.php
@@ -0,0 +1,33 @@
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-secondary navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn"
+        data-wow-delay="0.1s">
+        <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
+            <h1 class="mb-0 text-primary text-uppercase"><i class="fa fa-cut me-3"></i>The shortcut</h1>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="book.html" class="nav-item nav-link">Book</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Service</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="price.html" class="dropdown-item">Haircaut</a>
+                        <a href="team.html" class="dropdown-item">Coloring</a>
+                        <a href="open.html" class="dropdown-item">Smoothing</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                    </div>
+                </div>
+                <a href="service.html" class="nav-item nav-link">Barber Man</a>
+                <a href="service.html" class="nav-item nav-link">About Us</a>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block"><i
+                    class="fa fa-arrow-left me-3"></i> Sign Up</a>
+            <a href="" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3">Sign In<i
+                    class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
