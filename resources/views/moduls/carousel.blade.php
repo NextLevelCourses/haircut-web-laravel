6e96ea57041032ef05b9f5450d8b23678df0d28a
teguhafrianda <teguhafrianda8@gmail.com>
2025-06-20 19:26:23 +0700

    Perubahan pada carousel.blade.php

--- resources/views/moduls/carousel.blade.php
+++ resources/views/moduls/carousel.blade.php
@@ -3,10 +3,6 @@
 @section('title', 'Home')
 
 @section('content')
-    <!-- Carousel -->
-    <!-- About -->
-    <!-- etc. -->
-
 <!-- Carousel Start -->
     <div class="container-fluid p-0 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
