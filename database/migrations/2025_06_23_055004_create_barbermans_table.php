a93f1a63481db8135dd2d3086000aebc4fa7579c
xamppsul <samsul.marif100@gmail.com>
2025-06-23 13:35:42 +0800

    migration: create 1. barberman 2. genders 3. services 4. schedules

---
+++ database/migrations/2025_06_23_055004_create_barbermans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barbermans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('genders_id')->references('id')->on('genders')->onDelete('cascade');
+            $table->integer('price')->default(0);
+            $table->foreignId('services_id')->references('id')->on('services')->onDelete('cascade');
+            $table->integer('rating')->default(0);
+            $table->integer('no_hp')->unique();
+            $table->string('email')->unique();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barbermans');
+    }
+};
