c07d97e82b61b4835577ed7f8ac9d091b1a52dca
xamppsul <samsul.marif100@gmail.com>
2025-06-25 10:07:56 +0800

    annotation method do: login and register

--- app/Module/Auth/Handler/Auth_handler.php
+++ app/Module/Auth/Handler/Auth_handler.php
@@ -6,6 +6,13 @@ use App\Module\Auth\Interface\Auth_interface;
 
 class Auth_handler implements Auth_interface
 {
+    /**
+     * @method do_login
+     */
     public function do_login() {}
+
+    /**
+     * @method do_register
+     */
     public function do_register() {}
 }
