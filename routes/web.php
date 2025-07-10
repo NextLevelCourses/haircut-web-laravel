0a1b127625a8c1568ae1be8e2a21ef989fcb4374
teguhafrianda <teguhafrianda8@gmail.com>
2025-07-10 21:07:49 +0700

    fix confliq

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,4 @@
 <?php
 
-<<<<<<< HEAD
 require base_path('app/Module/Landing/Routes/web.php');
 require base_path('app/Module/Auth/Routes/web.php');
-=======
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ModulsController;
-
-Route::get('/', function () {
-    return view('moduls.carousel');
-})->name('home');
-
-Route::get('/service', [ModulsController::class, 'index']);
-Route::get('/gender', [ModulsController::class, 'gender']);
-Route::get('/barberman', [ModulsController::class, 'barberman']);
-Route::get('/jadwal', [ModulsController::class, 'jadwal']);
-Route::get('/transaction', [ModulsController::class, 'transaction']);
-Route::get('/transconfirm', [ModulsController::class, 'transconfirm']);
-Route::get('/failtransconfirm', [ModulsController::class, 'failtransconfirm']);
-Route::get('/login', [ModulsController::class, 'login']);
-Route::get('/register', [ModulsController::class, 'register']);
-Route::get('/testimoni', [ModulsController::class, 'testimoni']);
-Route::get('/history', [ModulsController::class, 'history']);
-Route::get('/invoice', [ModulsController::class, 'invoice']);
->>>>>>> development
