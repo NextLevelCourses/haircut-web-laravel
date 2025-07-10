ad6ebd5b71076e4943e037ae7f81c93f7125726f
xamppsul <samsul.marif100@gmail.com>
2025-07-10 23:04:45 +0800

    migration update:barbermans, and seeder add: barbermans,gender,services,schedules

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,10 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesSeeder::class,
             AccountSeeder::class,
+            ServiceSeeder::class,
+            GenderSeeder::class,
+            BarbermanSeeder::class,
+            ScheduleBabermanSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/GenderSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GenderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('genders')->insert([
+            [
+                'name' => 'Laki-laki',
+                'description' => 'Jenis kelamin laki-laki',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Perempuan',
+                'description' => 'Jenis kelamin perempuan',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
