e8dbcadfa7011c62e5be79a90b8423ce7f5b4139
xamppsul <samsul.marif100@gmail.com>
2025-07-14 18:45:44 +0800

    handler log success or error

--- app/Src/Log/Logging.php
+++ app/Src/Log/Logging.php
@@ -1,18 +1,32 @@
 <?php
 
-function HandlerLogSuccess() {}
+use Illuminate\Support\Facades\DB;
 
-
-function MainLog(string $type = 'error')
+function HandlerLogSuccess(string $message, int $user_id): void
 {
-    try {
-        switch ($type) {
-            case 'success':
-                break;
-            case 'error':
-                break;
-        }
-    } catch (Throwable $t) {
-        return 'Error: ' . $t->getMessage();
+    DB::insert('INSERT INTO log_success (message, users_id) VALUES (?, ?)', [
+        $message,
+        $user_id
+    ]);
+}
+
+function HandlerLogError(string $message, string $route, int $user_id): void
+{
+    DB::insert('INSERT INTO log_errors (message, route, users_id) VALUES (?, ?, ?)', [
+        $message,
+        $route,
+        $user_id
+    ]);
+}
+
+function MainLog(string $type = 'error', string $message, string $route, int $user_id): void
+{
+    switch ($type) {
+        case 'success':
+            HandlerLogSuccess($message, $user_id);
+            break;
+        case 'error':
+            HandlerLogError($message, $route, $user_id);
+            break;
     }
 }
