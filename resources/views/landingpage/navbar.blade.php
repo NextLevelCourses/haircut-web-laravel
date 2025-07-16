71752ef1c05e0079c3ebbb3be6f5cd5aba2d4d3a
xamppsul <samsul.marif100@gmail.com>
2025-07-16 17:41:34 +0800

    fix logout

--- resources/views/landingpage/navbar.blade.php
+++ resources/views/landingpage/navbar.blade.php
@@ -22,8 +22,14 @@
                 <a href="{{ url('/login') }}" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3">Log
                     In<i class="fa fa-sign-in ms-3"></i></a>
             @else
-                <a href="{{ url('/logout') }}" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3">Log
-                    Out<i class="fa fa-sign-out ms-3"></i></a>
+                <form action="{{ route('user.do_logout') }}" method="post" id="user.do_logout">
+                    @csrf
+                    <a href="{{ route('user.do_logout') }}"
+                        class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3"
+                        onclick="event.preventDefault(); document.getElementById('user.do_logout').submit();"
+                        role="button">Log
+                        Out<i class="fa fa-sign-out ms-3"></i></a>
+                </form>
             @endif
         </div>
     </nav>
