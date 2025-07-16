daa3db42c41b3cc6b16c6e0ab8ae08ef9675e332
xamppsul <samsul.marif100@gmail.com>
2025-07-16 10:58:46 +0800

    update navbar

--- resources/views/landingpage/master.blade.php
+++ resources/views/landingpage/master.blade.php
@@ -18,7 +18,7 @@
         rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
@@ -63,7 +63,7 @@
 
 
     <!-- Scripts -->
-     <!-- JavaScript Libraries -->
+    <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('assets/lib/wow/wow.min.js') }}"></script>
--- resources/views/landingpage/navbar.blade.php
+++ resources/views/landingpage/navbar.blade.php
@@ -17,9 +17,13 @@
                 <a href="#contact" class="nav-item nav-link">Contact</a>
             </div>
             <a href="{{ url('/register') }}" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block"><i
-                    class="fa fa-arrow-left me-3"></i> Sign Up</a>
-            <a href="{{ url('/login') }}" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3">Sign In<i
-                    class="fa fa-arrow-right ms-3"></i></a>
+                    class="fa fa-registered me-3"></i> Register</a>
+            <a href="{{ url('/login') }}" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3">Log
+                In<i class="fa fa-sign-in ms-3"></i></a>
+            @if (Auth::guard('user')->check())
+                <a href="{{ url('/logout') }}" class="btn btn-primary rounded-0 py-2 px-lg-4 d-none d-lg-block ms-3">Log
+                    Out<i class="fa fa-sign-out ms-3"></i></a>
+            @endif
         </div>
     </nav>
     <!-- Navbar End -->
