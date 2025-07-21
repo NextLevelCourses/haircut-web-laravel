4fe884704e306398fad1f7cfa04fcacfe38667b6
xamppsul <samsul.marif100@gmail.com>
2025-07-21 11:44:54 +0800

    fix typo call method success and failed of transaction

--- app/Module/Landing/Routes/web.php
+++ app/Module/Landing/Routes/web.php
@@ -9,6 +9,6 @@ Route::get('/gender', [Landing_handler::class, 'gender'])->name('Landing.gender'
 Route::get('/barberman', [Landing_handler::class, 'barberman'])->name('Landing.barberman');
 Route::get('/schedule', [Landing_handler::class, 'schedule'])->name('Landing.schedule');
 Route::get('/transaction', [Landing_handler::class, 'transaction'])->name('Landing.transaction');
-Route::get('/success_transaction', [Landing_handler::class, 'success_transaction'])->name('Landing.success_transaction');
-Route::get('/failed_transaction', [Landing_handler::class, 'failed_transaction'])->name('Landing.failed_transaction');
+Route::get('/success_transaction', [Landing_handler::class, 'SuccessTransaction'])->name('Landing.success_transaction');
+Route::get('/failed_transaction', [Landing_handler::class, 'FailedTransaction'])->name('Landing.failed_transaction');
 Route::get('/contact', [Landing_handler::class, 'contact'])->name('Landing.contact');
