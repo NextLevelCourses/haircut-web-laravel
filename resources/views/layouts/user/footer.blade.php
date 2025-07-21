e842212bd1389e266d14c37a65da24cfd430b0fa
xamppsul <samsul.marif100@gmail.com>
2025-07-21 11:36:39 +0800

    enable get in touch for contact data

--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -3,7 +3,6 @@
 <div class="container-fluid bg-secondary text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="row g-5">
-            <!-- disable get in touch section
             <div class="col-lg-4 col-md-6">
                 <h4 class="text-uppercase mb-4">Get In Touch</h4>
                 <div class="d-flex align-items-center mb-2">
@@ -25,7 +24,6 @@
                     <span>info@example.com</span>
                 </div>
             </div>
-        -->
             <div class="col-lg-4 col-md-6">
                 <h4 class="text-uppercase mb-4">Quick Links</h4>
                 <a class="btn btn-link" href="">Book</a>
