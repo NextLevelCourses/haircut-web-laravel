c8f1df5e3119d3541d4dfdd0872ea2a81cc179fd
xamppsul <samsul.marif100@gmail.com>
2025-07-28 10:41:18 +0800

    update struct table password reset tokens

--- database/migrations/2025_06_23_052247_create_users_table.php
+++ database/migrations/2025_06_23_052247_create_users_table.php
@@ -24,7 +24,8 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->id();
+            $table->string('email');
             $table->string('token');
             $table->string('ip');
             $table->string('device');
