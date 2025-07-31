3ea127902c072163deba3b7b125800baea14e33d
xamppsul <samsul.marif100@gmail.com>
2025-07-31 12:32:14 +0800

    fix landing and footer

--- app/Module/Landing/Handler/Landing_handler.php
+++ app/Module/Landing/Handler/Landing_handler.php
@@ -32,7 +32,7 @@ class Landing_handler extends Landing_domain implements Landing_interface
     public function HandlerMapDataLanding(): array
     {
         return array(
-            'about_us' => DB::select("SELECT * FROM about_us")[0], //first data
+            'about_us' => DB::select("SELECT * FROM about_us"),
             'service' => DB::select("SELECT * FROM services"),
             'working_hour' => DB::select("SELECT * FROM working_hour"),
             'barberman' => DB::select("SELECT barbermans.*,
--- resources/views/layouts/user/footer.blade.php
+++ resources/views/layouts/user/footer.blade.php
@@ -9,19 +9,19 @@
                     <div class="btn-square bg-dark flex-shrink-0 me-3">
                         <span class="fa fa-map-marker-alt text-primary"></span>
                     </div>
-                    <span>{{ $about->location }}</span>
+                    <span>{{ !empty($about[0]->location) ? $about[0]->location : '-' }}</span>
                 </div>
                 <div class="d-flex align-items-center mb-2">
                     <div class="btn-square bg-dark flex-shrink-0 me-3">
                         <span class="fa fa-phone-alt text-primary"></span>
                     </div>
-                    <span>{{ $about->work_phone }}</span>
+                    <span>{{ !empty($about[0]->work_phone) ? $about[0]->work_phone : '-' }}</span>
                 </div>
                 <div class="d-flex align-items-center">
                     <div class="btn-square bg-dark flex-shrink-0 me-3">
                         <span class="fa fa-envelope-open text-primary"></span>
                     </div>
-                    <span>{{ $about->work_email }}</span>
+                    <span>{{ !empty($about[0]->work_email) ? $about[0]->work_email : '-' }}</span>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6">
